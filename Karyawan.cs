6df9835e8516be2ff1c12a8b34cc871ee1a07ab9
Vian <howrain1@gmail.com>
2019-07-02 12:03:50 +0700

    Initial commit

---
+++ Karyawan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanAsosiasi
+{
+    class Karyawan
+    {
+        public string Nama { get; set; }
+        public bool Status { get; set; }
+
+        public bool Login(Kartu kartu)
+        {
+            return kartu.isValid(this);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanAsosiasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kartu kartu = new Kartu();
+
+            Karyawan karyawan = new Karyawan();
+            karyawan.Nama = "Paijo";
+            karyawan.Status = true;
+
+            if (karyawan.Login(kartu))
+            {
+                Console.WriteLine("Data karyawan valid");
+            }
+            else
+            {
+                Console.WriteLine("Data karyawan tidak valid");
+            }
+            Console.ReadKey();
+        }
+    }
+}
